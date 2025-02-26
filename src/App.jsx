7390a1ce209758998997d38d9d5390c45dd10748
illurihemanth <illurideephemanth@gmail.com>
2025-02-26 15:32:14 +0000

    updates_version

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,4 @@ import CardList from './components/CardList'
 
 import products from './data/full-products.json';
 
-// App.jsx
-import { Route, Routes} from 'react-router-dom';
 
-// ...
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      
-        <Routes>
-          <Route path="/" element={<CardList data={productData} />} />
-          <Route path="/product/:id" element={<SingleView data={productData} />} />
-        </Routes>
-      
-    </div>
-  );
-}
