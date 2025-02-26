43f00836c8416498f2a7fcf7b206f3d468efbbb2
illurihemanth <illurideephemanth@gmail.com>
2025-02-26 07:12:32 +0000

    updated_version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import CardList from './components/CardList';
-
 // App.jsx
+import Header from './components/Header';
+
+import productData from './data/full-products';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <CardList /> {/* Now rendering the imported CardList component */}
+    <div className="App">
+      <Header />
     </div>
   );
 }
 
 export default App;
 
+import CardList from './components/CardList'
+
+import products from './data/full-products.json';
+
+// App.jsx
+import { Route, Routes} from 'react-router-dom';
+
+// ...
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+        <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+      
+    </div>
+  );
+}
